572e5f2535e8bb39dbe1b93e5ef7f609c2a2eebc
skylarstjohns <skylar.johns@protonmail.com>
2023-04-04 14:09:07 -0600

    python client to connect via websocket

--- voxx-client/src/main/java/PythonClient.py
+++ voxx-client/src/main/java/PythonClient.py
@@ -1,51 +1,46 @@
-import socket
-import threading
 import tkinter as tk
+import asyncio
+import websockets
 
-# Initialize host and port
-HOST = 'localhost'
-PORT = 1027
-
-# Create socket object
-py_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# Bind socket to the address
-py_socket.bind((HOST, PORT))
-
-# Listen for incoming connections
-py_socket.listen()
-
-# Create an empty list to hold client threads
-threads = []
+# Define the WebSocket server URL and port
+SERVER_URL = "ws://localhost:8000/ws"
 
 
-# Define a function to handle each new client connection
-def handle_client(client_socket, address):
-    while True:
-        # Receive message from client
-        message = client_socket.recv(1024).decode()
-
-        # Send message to all clients
-        for thread in threads:
-            if thread != threading.current_thread():
-                thread.client_socket.send(f'{address[0]}: {message}'.encode())
+async def send_message(websocket, message):
+    """
+    Sends a message to the WebSocket server.
+    """
+    await websocket.send("PY|" + websocket.username + "|" + message).encode("utf-8"))
 
 
-# Define a function to accept incoming connections and start a new thread for each client
+async def receive_message(websocket):
+    """
+    Receives a message from the WebSocket server.
+    """
+    message = await websocket.recv()
+    return message
 
-def accept_connections():
-    while True:
-        # Accept incoming connection
-        client_socket, address = py_socket.accept()
 
-        # Create a new thread for the client and start it
-        thread = threading.Thread(target=handle_client, args=(client_socket, address))
-        thread.client_socket = client_socket
-        thread.start()
+async def main():
+    # Connect to the WebSocket server
+    async with websockets.connect(SERVER_URL) as websocket:
+        # Send a message to the JavaFX client
+        await send_message((websocket,  "PY|" + websocket.username + "|" + message).encode("utf-8"))
 
-        # Add the new thread to the list of client threads
-        threads.append(thread)
+        # Receive a message from the JavaFX client
+        response = await receive_message(websocket)
+        print(response)
 
+        # Send a message to the Java server
+        await send_message(websocket, message)
+
+        # Receive a message from the Java server
+        response = await receive_message(websocket)
+        print(response)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
 
 # Create GUI
 root = tk.Tk()
@@ -55,19 +50,6 @@ input_field = tk.Entry(root)
 input_field.pack()
 send_button = tk.Button(root, text='Send', command=lambda: send_message())
 send_button.pack()
-
-
-# Define message sending function
-def send_message():
-    message = input_field.get()
-    for thread in threads:
-        thread.client_socket.send(f'{HOST}: {message}'.encode())
-    input_field.delete(0, tk.END)
-
-
-# Create and start thread for accepting connections
-accept_thread = threading.Thread(target=accept_connections)
-accept_thread.start()
 
 # Start GUI
 root.mainloop()
